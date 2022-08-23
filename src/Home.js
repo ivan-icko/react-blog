f933cd7d42aacb3c911b5ba1b813e0a79a12902b
ivan-icko <ivanstepanovic6@gmail.com>
2022-08-23 21:58:33 +0200

    Zapocet rad sa useState atributima

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,29 @@
+import {useState} from 'react';
+
+
 const Home = () => {
+
+    //let name = 'Mario';
+    const [name,setName] = useState('Mario');
+    const [age,setAge] = useState(25);
+
+
+    const handleClick = ()=>{
+        setName('icko');
+        setAge(30);
+    }
+    const handleClickAgain = (name,e)=>{
+        console.log('hello'+name,e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} and {age}</p>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e)=>{handleClickAgain('icko',e)}}>Click me again</button>
         </div>
-     );
+     ); 
 }
  
 export default Home;
