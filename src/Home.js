0cdb3488bf3abf5cd902b0178c88f6a3a822f6ac
ivan-icko <ivanstepanovic6@gmail.com>
2022-08-24 21:03:34 +0200

    Kreiranje json servera

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,47 @@
-import {useState} from 'react';
-
+import { useState,useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new websige 1",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "My new websige 2",
+      body: "lorem ipsum...",
+      author: "icko",
+      id: 2,
+    },
+    {
+      title: "My new websige 3",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-    //let name = 'Mario';
-    const [name,setName] = useState('Mario');
-    const [age,setAge] = useState(25);
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
+  const [name,setName] = useState('mario');
 
-    const handleClick = ()=>{
-        setName('icko');
-        setAge(30);
-    }
-    const handleClickAgain = (name,e)=>{
-        console.log('hello'+name,e.target);
-    }
+  useEffect(()=>{
+        console.log('Use effect run');
+        console.log({name});
+  },[name]);
 
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <p>{name} and {age}</p>
-            <button onClick={handleClick}>Click me</button>
-            <button onClick={(e)=>{handleClickAgain('icko',e)}}>Click me again</button>
-        </div>
-     ); 
-}
- 
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
+      <button onClick={()=>{setName('luigi')}}>Change name</button>
+        <p>{name}</p>
+    </div>
+  );
+};
+
 export default Home;
